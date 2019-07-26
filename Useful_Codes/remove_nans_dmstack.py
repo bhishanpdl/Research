b122df85f525bfce8f79197fd304cada00ed93af
Bhishan Poudel <bhishanpdl@users.noreply.github.com>
2019-07-26 11:05:35 -0400

    Add files via upload

--- Useful_Codes/remove_nans_dmstack.py
+++ Useful_Codes/remove_nans_dmstack.py
@@ -1,47 +1,28 @@
-#!/bin/env python
-"""
-Remove nans from textfile output of dmstack and only extract few columns
- Author: Bhishan Poudel
-
- Filtering:
- 1. flag calib_psfCandidate==False
- 2. column deblend_nChild==0
- 3. ellipticity e =  sqrt(e1^2 + e2^2) < 1.5
- 4. choose only few columns given below
- 5. remove nans from all these columns
- 6. change delimiter to tab.
-
-Input columns:
-    SN  Column: Name
-    0    1   :  calib_psfCandidate (for filtering only)
-    1    94  :  deblend_nChild (for filtering only)
-    2    90  :  id
-    3    102 :  base_SdssCentroid_x
-    4    103 :  base_SdssCentroid_y
-    5    104 :  base_SdssCentroid_xSigma
-    6    105 :  base_SdssCentroid_ySigma
-    7    127 :  ext_shapeHSM_HsmShapeRegauss_e1
-    8    128 :  ext_shapeHSM_HsmShapeRegauss_e2
-    9    114 :  ext_shapeHSM_HsmShapeRegauss_sigma
-
-
-Output columns:
-    cols_select = ['file_number', 'id',
-           'base_SdssCentroid_x', 'base_SdssCentroid_y',
-           'base_SdssCentroid_xSigma','base_SdssCentroid_ySigma',
-           'ext_shapeHSM_HsmShapeRegauss_e1','ext_shapeHSM_HsmShapeRegauss_e2',
-           'base_SdssShape_flux']
-
- We run this script on the folder having dmstack output csv files and all the csv files will be converted
- into new .txt files.
-
- Example:
- From: src_lsst90_z1.5_099.csv # these files are obtained from dmstack
- To:     src_lsst90_z1.5_099.txt
-
- python remove_nans.dmstack.py
-"""
-
+# Remove nans from textfile output of dmstack and only extract few columns
+# Author: Bhishan Poudel
+#
+# Filtering:
+# 1. flag calib_psfCandidate==False
+# 2. column deblend_nChild==0
+# 3. ellipticity e =  sqrt(e1^2 + e2^2) < 1.5
+# 4. choose only few columns given below
+# 5. remove nans from all these columns
+# 6. change delimiter to tab.
+#
+# columns:
+# id (90)
+# base_SdssCentroid_x, base_SdssCentroid_y (102, 103)
+# base_SdssCentroid_xSigma, base_SdssCentroid_ySigma (104,105)
+# ext_shapeHSM_HsmShapeRegauss_e1, ext_shapeHSM_HsmShapeRegauss_e2 (127, 128)
+# base_SdssShape_flux (114)
+#
+# In total there are 1+8 columns
+# file_number
+# id
+# x1,x2 xerr1 xerr2
+# e1 e2
+# flux
+#
 import pandas as pd
 import numpy as np
 import sys
@@ -50,27 +31,40 @@ import glob
 def remove_nans(ifile,file_number):
     """ Remove nans and filter data from dmstack output csv file.
 
-    Original dmstack output has 77 columns. First column is flags and second is id and last is footprint.
-    First column has 90 flags in it, and we expand this column to get 90 + 76 = 166 columns.
-    In python numbering starts from 0 so column 90 becomes id column after expanding columns.
+    There are 90 flags col0 to col89
+    col90 is id is first column 'id'
 
-    SN  Column: Name
-    0    1   :  calib_psfCandidate (for filtering only)
-    1    94  :  deblend_nChild (for filtering only)
-    2    90  :  id
-    3    102 :  base_SdssCentroid_x
-    4    103 :  base_SdssCentroid_y
-    5    104 :  base_SdssCentroid_xSigma
-    6    105 :  base_SdssCentroid_ySigma
-    7    127 :  ext_shapeHSM_HsmShapeRegauss_e1
-    8    128 :  ext_shapeHSM_HsmShapeRegauss_e2
-    9    114 :  ext_shapeHSM_HsmShapeRegauss_sigma
+    There are 90 flags and 77 columns.
+    We exclude first column 'flags' and have 76 columns
+    In total there are 90 + 76 = 166 columns.
 
-    In the output we also add fileNumber column.
+    Columns selected:
+    1   :  calib_psfCandidate (for filtering only)
+    94  :  deblend_nChild (for filtering only)
+    90  :  id
+    102 :  base_SdssCentroid_x
+    103 :  base_SdssCentroid_y
+    104 :  base_SdssCentroid_xSigma
+    105 :  base_SdssCentroid_ySigma
+    127 :  ext_shapeHSM_HsmShapeRegauss_e1
+    128 :  ext_shapeHSM_HsmShapeRegauss_e2
+    114 :  ext_shapeHSM_HsmShapeRegauss_sigma
+
+    # Added later for radius calculation
+    133: 'ext_shapeHSM_HsmSourceMoments_xx',
+    134: 'ext_shapeHSM_HsmSourceMoments_yy',
+    135: 'ext_shapeHSM_HsmSourceMoments_xy',
+
+    # This gives
+    radius = (xx*xy - yy**2)**1/4
+
+    # In the output  file we have
+    file_number, id,  x,y xsigma, ysigma, e1, e2, flux, radius
     """
 
-    usecols = [1, 94, 90, 102, 103, 104, 105, 127, 128, 114]
-    df = pd.read_csv(ifile, sep=",",low_memory=False)
+    usecols = [1, 94, 90, 102, 103, 104, 105, 127, 128, 114, 133,134,135]
+
+    df = pd.read_csv(ifile, sep=",",low_memory=False,usecols=usecols)
 
 
     for c in df.columns:
@@ -89,8 +83,12 @@ def remove_nans(ifile,file_number):
     # clean out unphysical results
     # e1^2 + e2^2 < 1.5^2
     df['e'] = (df['ext_shapeHSM_HsmShapeRegauss_e1'] ** 2 + df['ext_shapeHSM_HsmShapeRegauss_e2'] ** 2)**0.5
-
     df = df.query('e < 1.5')
+
+    # calculate radius of ellipse using HSM moments
+    # radius**4 = xx*yy - xy**2
+    df['radius'] =  df.eval(""" ( (ext_shapeHSM_HsmSourceMoments_xx *  ext_shapeHSM_HsmSourceMoments_yy) \
+                                              -  (ext_shapeHSM_HsmSourceMoments_xy**2 ) )**0.25 """)
 
     # add a new column with file_number
     df['file_number'] = file_number
@@ -100,7 +98,8 @@ def remove_nans(ifile,file_number):
            'base_SdssCentroid_x', 'base_SdssCentroid_y',
            'base_SdssCentroid_xSigma','base_SdssCentroid_ySigma',
            'ext_shapeHSM_HsmShapeRegauss_e1','ext_shapeHSM_HsmShapeRegauss_e2',
-           'base_SdssShape_flux']
+           'base_SdssShape_flux',  'radius',
+           ]
 
     df = df[cols_select]
 
@@ -108,7 +107,7 @@ def remove_nans(ifile,file_number):
     df = df.dropna()
 
     # write txt file with commented header
-    prefix = ' '*11
+    prefix = ' '*2
     header_line = prefix.join(cols_select)
 
     ofile = ifile[0:-4] + '.txt'
